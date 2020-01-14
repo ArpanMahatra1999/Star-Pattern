6a8b9fd40682a4cbd2820535698b1bb85f8f0f00
Arpan Mahatra <47106999+ArpanMahatra1999@users.noreply.github.com>
2020-01-14 14:12:14 +0545

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,11 @@
+def pattern(n):
+    k = 2*n - 2
+    for i in range(0,n): #for rows
+        for j in range(0,k): #for columns
+            print(end=" ")
+        k = k - 1
+        for j in range(0,i+1):
+            print("* ",end=" ")
+        print(("\r"))
+
+pattern(5)
