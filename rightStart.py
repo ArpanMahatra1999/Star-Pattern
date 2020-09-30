4fdbeabc61e37f907de3340083dcf4b526d61e37
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2020-09-30 21:17:10 +0530

    Update rightStart.py

--- rightStart.py
+++ rightStart.py
@@ -1,3 +1,14 @@
+#FOR ABSOLUTE BEGINNERS 
+rows = int(input("ENTER THE NUMBERS OF ROWS YOU WANT : "))
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+
+    print("\r")
+
+    
+    
+#FOR MODERATE LEARNERS
 def pattern(n):
     for i in range(0,n):
         for j in range(0,i+1):
